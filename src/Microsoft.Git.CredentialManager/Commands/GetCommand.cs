fa3361fe8a8c029c5ed43363bcf12974065f915e
Matthew John Cheetham <mjcheetham@outlook.com>
2018-11-20 22:36:02 +0000

    Implement get command

--- src/Microsoft.Git.CredentialManager/Commands/GetCommand.cs
+++ src/Microsoft.Git.CredentialManager/Commands/GetCommand.cs
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Git.CredentialManager.SecureStorage;
 
 namespace Microsoft.Git.CredentialManager.Commands
 {
@@ -15,9 +16,56 @@ namespace Microsoft.Git.CredentialManager.Commands
 
         protected override string Name => "get";
 
-        protected override Task ExecuteInternalAsync(ICommandContext context, InputArguments input, IHostProvider provider, string credentialKey)
+        protected override async Task ExecuteInternalAsync(ICommandContext context, InputArguments input, IHostProvider provider, string credentialKey)
         {
-            throw new NotImplementedException();
+            // Try and locate an existing PAT in the OS credential store
+            context.Trace.WriteLine("Looking for existing credential in store...");
+            ICredential credential = context.CredentialStore.Get(credentialKey);
+
+            if (credential == null)
+            {
+                context.Trace.WriteLine("No existing credential found.");
+
+                // No existing PAT was found.
+                // Create a new one and add this to the store.
+                context.Trace.WriteLine("Creating new credential...");
+                credential = await provider.CreateCredentialAsync(input);
+                context.Trace.WriteLine("Credential created.");
+
+                if (provider.IsCredentialStoredOnCreation)
+                {
+                    context.Trace.WriteLine("Storing credential...");
+                    context.CredentialStore.AddOrUpdate(credentialKey, credential);
+                    context.Trace.WriteLine("Credential stored.");
+                }
+            }
+            else
+            {
+                context.Trace.WriteLine("Existing credential found.");
+            }
+
+            var output = new Dictionary<string, string>();
+
+            // Echo protocol, host, and path back at Git
+            if (input.Protocol != null)
+            {
+                output["protocol"] = input.Protocol;
+            }
+            if (input.Host != null)
+            {
+                output["host"] = input.Host;
+            }
+            if (input.Path != null)
+            {
+                output["path"] = input.Path;
+            }
+
+            // Return the credential to Git
+            output["username"] = credential.UserName;
+            output["password"] = credential.Password;
+
+            // Write the values to standard out
+            context.StdOut.WriteDictionary(output);
         }
     }
 }
