2470ff8f1fb9fd3dc338c77aba8ca4d8082dd64f
Matthew John Cheetham <mjcheetham@outlook.com>
2018-11-20 22:30:51 +0000

    Implement the erase and store commands
    Implement the erase and store commands using the secure credential
    store APIs.

--- src/Microsoft.Git.CredentialManager/Commands/StoreCommand.cs
+++ src/Microsoft.Git.CredentialManager/Commands/StoreCommand.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-using System;
 using System.Threading.Tasks;
 
 namespace Microsoft.Git.CredentialManager.Commands
@@ -17,7 +16,28 @@ namespace Microsoft.Git.CredentialManager.Commands
 
         protected override Task ExecuteInternalAsync(ICommandContext context, InputArguments input, IHostProvider provider, string credentialKey)
         {
-            throw new NotImplementedException();
+            // Some providers may choose to store the credential when initially asked for it during
+            // a previous `get` call (where there might be more information available in the context
+            // or input which is no longer present).
+            // To prevent 'double stores' we should ask the provider if they store on create.
+            if (!provider.IsCredentialStoredOnCreation)
+            {
+                // Create the credential based on Git's input
+                string userName = input.UserName;
+                string password = input.Password;
+                var credential = new GitCredential(userName, password);
+
+                // Add or update the credential in the store.
+                context.Trace.WriteLine("Storing credential...");
+                context.CredentialStore.AddOrUpdate(credentialKey, credential);
+                context.Trace.WriteLine("Credential was successfully stored.");
+            }
+            else
+            {
+                context.Trace.WriteLine("Skipping 'store' because provider stores credentials on 'get'.");
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
