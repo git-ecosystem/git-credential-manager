3f90c6ca2190743026c7325a54c072dc196946de
Matthew John Cheetham <mjcheetham@outlook.com>
2019-03-04 13:20:12 +0000

    Refactor repo layout, simplify build and use NB.GitVersioning
    Change the repository layout to group all the source and test files
    under the src/ directory, then split by platform (windows, osx, and
    shared) at the next level.
    
    Redirect the build output for all the projects to the top-level out/
    directory. This means it's easier to do the path-math for signing and
    building the installers.
    
    Use Nerdbank.GitVersioning to provide a unified Semantic Version model
    for all of our assemblies and installers.
    
    Integrate the Mac-native MSAuth helper Xcode workspace into the main
    .NET build by chaining the build into the main CLI .NET project so that
    MSBuild shells out to xcodebuild.
    
    Also create a wrapper .NET project for the Mac installer script with a
    two step process: 'layout.sh' for making the installer payload layout,
    and 'pack.sh' for constructing the pkg file. Building the MSBuild project
    will call both layout and pack to produce an unsigned pkg file for
    development purposes. The signed release build can call 'layout.sh' and
    'pack.sh' separately (and do signing of binaries in between).

---
+++ src/osx/Microsoft.Authentication.Helper.Mac/build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+die () {
+    echo "$*" >&2
+    exit 1
+}
+
+echo "Building Microsoft.Authentication.Helper.Mac..."
+
+# Directories
+THISDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+ROOT="$( cd $THISDIR/../../.. ; pwd -P )"
+SRC=$ROOT/src
+OUT=$ROOT/out
+MSAUTH_SRC=$SRC/osx/Microsoft.Authentication.Helper.Mac
+MSAUTH_OUT=$OUT/osx/Microsoft.Authentication.Helper.Mac
+
+# Parse script arguments
+for i in "$@"
+do
+case $i in
+    --configuration=*)
+    CONFIGURATION="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+# Set default arguments
+CONFIGURATION=${CONFIGURATION:=Debug}
+
+MSAUTH_BINOUT=$MSAUTH_OUT/bin/$CONFIGURATION/native
+MSAUTH_OBJOUT=$MSAUTH_OUT/xcodebuild
+
+# Ensure output directories exist
+mkdir -p $MSAUTH_OUT || exit 1
+
+# # Ensure Cocoapods have been restored (try without updating the repo first as this can take a while)
+echo "Restoring Cocoapods..."
+pod install --project-directory=$MSAUTH_SRC
+if [ $? -ne 0 ]; then
+    echo "Failed to restore Cocoapods. Will update Cocoapods repository and try again..."
+    pod install --repo-update --project-directory=$MSAUTH_SRC || exit 1
+fi
+
+# Build the Xcode workspace
+echo "Building Xcode workspace..."
+xcodebuild \
+    -workspace $MSAUTH_SRC/Microsoft.Authentication.Helper.xcworkspace \
+    -scheme Microsoft.Authentication.Helper \
+    -configuration $CONFIGURATION \
+    -derivedDataPath $MSAUTH_OBJOUT || exit 1
+
+# Copy binaries
+echo "Copying binaries..."
+MSAUTH_EXEC=$MSAUTH_OBJOUT/Build/Products/$CONFIGURATION/Microsoft.Authentication.Helper
+mkdir -p $MSAUTH_BINOUT || exit 1
+cp $MSAUTH_EXEC $MSAUTH_BINOUT || exit 1
+
+echo "Build of Microsoft.Authentication.Helper.Mac complete."
