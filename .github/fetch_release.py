53abdc5f55d95f546ca4cbaab2d85915949cc6a5
Kyle Rader (HE|HIM) <kyrader@microsoft.com>
2020-11-17 03:10:43 -0800

    print debugging

--- .github/fetch_release.py
+++ .github/fetch_release.py
@@ -1,21 +1,30 @@
 from os import environ as env
 import requests
 import time
+import json
 
 release = env['RELEASE'] if 'RELEASE' in env else "latest"
 release_arg = "latest" if release == "latest" else f"tags/{release}"
+
+print(f"release_arg set to {release_arg}")
+
 release_url = f"https://api.github.com/repos/microsoft/git-credential-manager-core/releases/{release_arg}"
 
 try:
+    print(f"Fetching release {release_url}")
     r = requests.get(release_url)
 
     # if at first you don't succeed...
     if r.status_code != 200:
+        print(f"Trying again, initial status {r.status_code}")
         time.sleep(2)
         r = requests.get(release_url)
 
+    print("Response:")
+    print(json.dumps(r.json(), indent=4))
+
     if r.status_code != 200:
-        raise Error(f"Failed to fetch release info from {release_url}")
+        raise ValueError(f"Failed to fetch release info from {release_url}")
 
     asset = [a for a in r.json()['assets'] if a['name'].endswith(".deb")][0]
     asset_url = asset['browser_download_url']
