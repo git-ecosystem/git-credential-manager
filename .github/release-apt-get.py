61accd7febba0e946e9043cbfc7c05417f89f865
Kyle Rader (HE|HIM) <kyrader@microsoft.com>
2020-11-17 02:04:52 -0800

    Check env vars and include them from secrets

--- .github/release-apt-get.py
+++ .github/release-apt-get.py
@@ -1,5 +1,18 @@
 from os import enviorn as env
 
+
+def check_var(name:str) -> bool:
+    if not env[name]:
+        print(f"Required env var {name} is missing!")
+
+
+for var in ['APT_REPO_ID', 'AZURE_AAD_ID', 'AAD_CLIENT_SECRET']:
+    check_var(var)
+
+repo_id = env['APT_REPO_ID']
+aad_id = env['AZURE_AAD_ID']
+password = env['AAD_CLIENT_SECRET']
+
 repo_config = {
     "AADResource": "https://microsoft.onmicrosoft.com/945999e9-da09-4b5b-878f-b66c414602c0",
     "AADTenant": "72f988bf-86f1-41af-91ab-2d7cd011db47",
@@ -7,9 +20,9 @@ repo_config = {
     "server": "azure-apt-cat.cloudapp.net",
     "port": "443",
 
-    "repositoryId": env['APT_REPO_ID'],
-    "AADClientId": env['AZURE_AAD_ID'],
-    "AADClientSecret": env['AAD_CLIENT_SECRET']
+    "repositoryId": repo_id,
+    "AADClientId": aad_id,
+    "AADClientSecret": password,
 }
 
 configs = [
