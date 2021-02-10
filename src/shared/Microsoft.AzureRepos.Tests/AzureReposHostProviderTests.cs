mjcheetham/Git-Credential-Manager-Core

azrepos: add authority cache for AzDevOps orgs
Add a cache of AAD authorities for Azure DevOps organisations, using an
INI file.

To avoid making a HEAD request to the Azure DevOps instance each time we
want to perform AAD/MSA authentication in order to determine the
authority, we first check for a cached value and use that.

If there is an auth failure then we remove any cached authority for that
AzDevOps org in case the reason for failure was a token from the wrong
issuing authority. This can happen if an AzDevOps org changes its
backing authority from MSA/AAD or between AAD tenants.

src/shared/Microsoft.AzureRepos.Tests/AzureReposHostProviderTests.cs
@@ -132,8 +132,9 @@ namespace Microsoft.AzureRepos.Tests
             var context = new TestCommandContext();
             var azDevOps = Mock.Of<IAzureDevOpsRestApi>();
             var msAuth = Mock.Of<IMicrosoftAuthentication>();
+            var authorityCache = Mock.Of<IAzureDevOpsAuthorityCache>();
 
-            var provider = new AzureReposHostProvider(context, azDevOps, msAuth);
+            var provider = new AzureReposHostProvider(context, azDevOps, msAuth, authorityCache);
 
             await Assert.ThrowsAsync<Exception>(() => provider.GetCredentialAsync(input));
         }
@@ -170,7 +171,11 @@ namespace Microsoft.AzureRepos.Tests
             msAuthMock.Setup(x => x.GetTokenAsync(authorityUrl, expectedClientId, expectedRedirectUri, expectedScopes, null))
                       .ReturnsAsync(authResult);
 
-            var provider = new AzureReposHostProvider(context, azDevOpsMock.Object, msAuthMock.Object);
+
+            var authorityCacheMock = new Mock<IAzureDevOpsAuthorityCache>();
+            authorityCacheMock.Setup(x => x.GetAuthorityAsync("org")).ReturnsAsync(authorityUrl);
+
+            var provider = new AzureReposHostProvider(context, azDevOpsMock.Object, msAuthMock.Object, authorityCacheMock.Object);
 
             ICredential credential = await provider.GetCredentialAsync(input);
 

git-ecosystem/git-credential-manager

src/shared/Microsoft.AzureRepos.Tests/AzureReposHostProviderTests.cs
@@ -132,8 +132,9 @@ namespace Microsoft.AzureRepos.Tests
             var context = new TestCommandContext();
             var azDevOps = Mock.Of<IAzureDevOpsRestApi>();
             var msAuth = Mock.Of<IMicrosoftAuthentication>();
+            var authorityCache = Mock.Of<IAzureDevOpsAuthorityCache>();
 
-            var provider = new AzureReposHostProvider(context, azDevOps, msAuth);
+            var provider = new AzureReposHostProvider(context, azDevOps, msAuth, authorityCache);
 
             await Assert.ThrowsAsync<Exception>(() => provider.GetCredentialAsync(input));
         }
@@ -170,7 +171,11 @@ namespace Microsoft.AzureRepos.Tests
             msAuthMock.Setup(x => x.GetTokenAsync(authorityUrl, expectedClientId, expectedRedirectUri, expectedScopes, null))
                       .ReturnsAsync(authResult);
 
-            var provider = new AzureReposHostProvider(context, azDevOpsMock.Object, msAuthMock.Object);
+
+            var authorityCacheMock = new Mock<IAzureDevOpsAuthorityCache>();
+            authorityCacheMock.Setup(x => x.GetAuthority("org")).Returns(authorityUrl);
+
+            var provider = new AzureReposHostProvider(context, azDevOpsMock.Object, msAuthMock.Object, authorityCacheMock.Object);
 
             ICredential credential = await provider.GetCredentialAsync(input);
 