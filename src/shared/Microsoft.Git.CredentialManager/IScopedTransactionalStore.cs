mjcheetham/Git-Credential-Manager-Core

ini: add INI file serialiser and file store components
Add an INI file serialiser type, and a file-backed INI store that
supports a very basic commit/rollbacks single transaction.

src/shared/Microsoft.Git.CredentialManager/IScopedTransactionalStore.cs
@@ -1,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.Git.CredentialManager
+{
+    /// <summary>
+    /// Represents a simple section-scoped property/value store that can accumulate changes before 'committing'
+    /// them to a persistent storage location, or be reloaded from that storage at will.
+    /// </summary>
+    public interface IScopedTransactionalStore
+    {
+        /// <summary>
+        /// Reload the store from persisted storage. Any uncommitted changes will be lost.
+        /// </summary>
+        Task ReloadAsync();
+
+        /// <summary>
+        /// Commit changes to persisted storage. Any changes made outside of the application will be overwritten.
+        /// </summary>
+        Task CommitAsync();
+
+        /// <summary>
+        /// Get all scopes present for the given section name.
+        /// </summary>
+        /// <param name="sectionName">Name of section.</param>
+        /// <returns>All scopes present for the specified section name in the store.</returns>
+        IEnumerable<string> GetSectionScopes(string sectionName);
+
+        /// <summary>
+        /// Try to get the value of a property for the given key.
+        /// </summary>
+        /// <param name="key">Property key.</param>
+        /// <param name="value">Property value.</param>
+        /// <returns>True if a property with the specified key exists, false otherwise.</returns>
+        bool TryGetValue(string key, out string value);
+
+        /// <summary>
+        /// Set the value of a property for the given key. Existing values are overwritten.
+        /// </summary>
+        /// <param name="key">Property key.</param>
+        /// <param name="value">Property value.</param>
+        void SetValue(string key, string value);
+
+        /// <summary>
+        /// Delete a property identified by the given key if it exists.
+        /// </summary>
+        /// <param name="key">Property key.</param>
+        void Remove(string key);
+    }
+}

src/shared/Microsoft.Git.CredentialManager/IniFileStore.cs
@@ -1,0 +1,166 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Git.CredentialManager
+{
+    /// <summary>
+    /// Scoped transactional store that uses an INI file as the persistent storage.
+    /// </summary>
+    public class IniFileStore : IScopedTransactionalStore
+    {
+        private readonly IFileSystem _fileSystem;
+        private readonly IniSerializer _serializer;
+        private readonly string _filePath;
+        private readonly string _parentPath;
+        private readonly object _fileLock = new object();
+        private IniFile _iniFile;
+
+        public IniFileStore(IFileSystem fileSystem, IniSerializer serializer, string filePath)
+        {
+            _fileSystem = fileSystem;
+            _serializer = serializer;
+            _filePath = filePath;
+            _parentPath = Path.GetDirectoryName(_filePath);
+
+            ReloadAsync();
+        }
+
+        public Task ReloadAsync()
+        {
+            lock (_fileLock)
+            {
+                if (_fileSystem.FileExists(_filePath))
+                {
+                    using (Stream fs = _fileSystem.OpenFileStream(_filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    using (var reader = new StreamReader(fs))
+                    {
+                        _iniFile = _serializer.Deserialize(reader);
+                    }
+                }
+                else
+                {
+                    // Create a new empty INI file object
+                    _iniFile = new IniFile();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task CommitAsync()
+        {
+            lock (_fileLock)
+            {
+                // Ensure parent directory exists
+                if (!_fileSystem.DirectoryExists(_parentPath))
+                {
+                    _fileSystem.CreateDirectory(_parentPath);
+                }
+
+                using (Stream fs = _fileSystem.OpenFileStream(_filePath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                using (var writer = new StreamWriter(fs))
+                {
+                    _serializer.Serialize(_iniFile, writer);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public IEnumerable<string> GetSectionScopes(string sectionName)
+        {
+            lock (_fileLock)
+            {
+                IEnumerable<IniSection> sections = _iniFile.Sections
+                    .Where(x => StringComparer.OrdinalIgnoreCase.Equals(x.Name, sectionName));
+                foreach (var section in sections)
+                {
+                    yield return section.Scope;
+                }
+            }
+        }
+
+        public bool TryGetValue(string key, out string value)
+        {
+            lock (_fileLock)
+            {
+                value = null;
+
+                if (!TrySplitKey(key, out string section, out string scope, out string property))
+                {
+                    throw new ArgumentException($"Invalid key '{key}'.", nameof(key));
+                }
+
+                return _iniFile.TryGetValue(section, scope, property, out value);
+            }
+        }
+
+        public void SetValue(string key, string value)
+        {
+            lock (_fileLock)
+            {
+                if (!TrySplitKey(key, out string section, out string scope, out string property))
+                {
+                    throw new ArgumentException($"Invalid key '{key}'.", nameof(key));
+                }
+
+                _iniFile.SetValue(section, scope, property, value);
+            }
+        }
+
+        public void Remove(string key)
+        {
+            lock (_fileLock)
+            {
+                if (!TrySplitKey(key, out string section, out string scope, out string property))
+                {
+                    throw new ArgumentException($"Invalid key '{key}'.", nameof(key));
+                }
+
+                _iniFile.UnsetValue(section, scope, property);
+            }
+        }
+
+        private static bool TrySplitKey(string key, out string section, out string scope, out string property)
+        {
+            section = null;
+            scope = null;
+            property = null;
+
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                return false;
+            }
+
+            int first = key.IndexOf('.');
+            int last = key.LastIndexOf('.');
+
+            if (first < 0 || last < 0)
+            {
+                return false;
+            }
+
+            // section.property
+            if (first == last)
+            {
+                section = key.Substring(0, first);
+                property = key.Substring(last + 1);
+
+                return true;
+            }
+
+            // section.scope.maybe.with.periods.property
+            section = key.Substring(0, first);
+            scope = key.Substring(first + 1, last - first - 1);
+            property = key.Substring(last + 1);
+
+            return true;
+        }
+    }
+}

src/shared/TestInfrastructure/Objects/InMemoryIniStore.cs
@@ -1,0 +1,99 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Git.CredentialManager.Tests.Objects
+{
+    public class InMemoryIniStore : IScopedTransactionalStore
+    {
+        private readonly object _txLock = new object();
+
+        public IDictionary<string, string> PersistedStore { get; }
+
+        public IDictionary<string, string> MemoryStore { get; }
+
+        public InMemoryIniStore() : this(EqualityComparer<string>.Default) { }
+
+        public InMemoryIniStore(IEqualityComparer<string> comparer)
+            : this(new Dictionary<string, string>(comparer)) { }
+
+        public InMemoryIniStore(Dictionary<string, string> persistedStore)
+        {
+            PersistedStore = persistedStore;
+            MemoryStore = new Dictionary<string, string>(persistedStore.Comparer);
+
+            ReloadAsync();
+        }
+
+        public Task ReloadAsync()
+        {
+            // Copy state from persisted store -> memory store
+            lock (_txLock)
+            {
+                MemoryStore.Clear();
+                foreach (var kvp in PersistedStore)
+                {
+                    MemoryStore.Add(kvp);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task CommitAsync()
+        {
+            // Copy state from memory store -> persisted store
+            lock (_txLock)
+            {
+                PersistedStore.Clear();
+                foreach (var kvp in MemoryStore)
+                {
+                    PersistedStore.Add(kvp);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public IEnumerable<string> GetSectionScopes(string sectionName)
+        {
+            var scopes = new HashSet<string>();
+
+            string prefix = $"{sectionName}.";
+            var cmp = StringComparison.OrdinalIgnoreCase;
+            foreach (string key in MemoryStore.Keys.Where(x => x.StartsWith(prefix, cmp)))
+            {
+                int sectionIndex = key.IndexOf(".", cmp);
+                int propertyIndex = key.LastIndexOf(".", cmp);
+
+                if (sectionIndex > -1 && propertyIndex > -1)
+                {
+                    string scope = key.Substring(sectionIndex + 1,
+                        propertyIndex - sectionIndex - 1);
+                    scopes.Add(scope);
+                }
+            }
+
+            return scopes;
+        }
+
+        public bool TryGetValue(string key, out string value)
+        {
+            return MemoryStore.TryGetValue(key, out value);
+        }
+
+        public void SetValue(string key, string value)
+        {
+            MemoryStore[key] = value;
+        }
+
+        public void Remove(string key)
+        {
+            MemoryStore.Remove(key);
+        }
+    }
+}

git-ecosystem/git-credential-manager

src/shared/Microsoft.Git.CredentialManager/IScopedTransactionalStore.cs
@@ -1,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.Git.CredentialManager
+{
+    /// <summary>
+    /// Represents a simple section-scoped property/value store that can accumulate changes before 'committing'
+    /// them to a persistent storage location, or be reloaded from that storage at will.
+    /// </summary>
+    public interface IScopedTransactionalStore
+    {
+        /// <summary>
+        /// Reload the store from persisted storage. Any uncommitted changes will be lost.
+        /// </summary>
+        Task Reload();
+
+        /// <summary>
+        /// Commit changes to persisted storage. Any changes made outside of the application will be overwritten.
+        /// </summary>
+        Task Commit();
+
+        /// <summary>
+        /// Get all scopes present for the given section name.
+        /// </summary>
+        /// <param name="sectionName">Name of section.</param>
+        /// <returns>All scopes present for the specified section name in the store.</returns>
+        IEnumerable<string> GetSectionScopes(string sectionName);
+
+        /// <summary>
+        /// Try to get the value of a property for the given key.
+        /// </summary>
+        /// <param name="key">Property key.</param>
+        /// <param name="value">Property value.</param>
+        /// <returns>True if a property with the specified key exists, false otherwise.</returns>
+        bool TryGetValue(string key, out string value);
+
+        /// <summary>
+        /// Set the value of a property for the given key. Existing values are overwritten.
+        /// </summary>
+        /// <param name="key">Property key.</param>
+        /// <param name="value">Property value.</param>
+        void SetValue(string key, string value);
+
+        /// <summary>
+        /// Delete a property identified by the given key if it exists.
+        /// </summary>
+        /// <param name="key">Property key.</param>
+        void Remove(string key);
+    }
+}

src/shared/Microsoft.Git.CredentialManager/IniFileStore.cs
@@ -1,0 +1,166 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Git.CredentialManager
+{
+    /// <summary>
+    /// Scoped transactional store that uses an INI file as the persistent storage.
+    /// </summary>
+    public class IniFileStore : IScopedTransactionalStore
+    {
+        private readonly IFileSystem _fileSystem;
+        private readonly IniSerializer _serializer;
+        private readonly string _filePath;
+        private readonly string _parentPath;
+        private readonly object _fileLock = new object();
+        private IniFile _iniFile;
+
+        public IniFileStore(IFileSystem fileSystem, IniSerializer serializer, string filePath)
+        {
+            _fileSystem = fileSystem;
+            _serializer = serializer;
+            _filePath = filePath;
+            _parentPath = Path.GetDirectoryName(_filePath);
+
+            Reload();
+        }
+
+        public Task Reload()
+        {
+            lock (_fileLock)
+            {
+                if (_fileSystem.FileExists(_filePath))
+                {
+                    using (Stream fs = _fileSystem.OpenFileStream(_filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    using (var reader = new StreamReader(fs))
+                    {
+                        _iniFile = _serializer.Deserialize(reader);
+                    }
+                }
+                else
+                {
+                    // Create a new empty INI file object
+                    _iniFile = new IniFile();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task Commit()
+        {
+            lock (_fileLock)
+            {
+                // Ensure parent directory exists
+                if (!_fileSystem.DirectoryExists(_parentPath))
+                {
+                    _fileSystem.CreateDirectory(_parentPath);
+                }
+
+                using (Stream fs = _fileSystem.OpenFileStream(_filePath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                using (var writer = new StreamWriter(fs))
+                {
+                    _serializer.Serialize(_iniFile, writer);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public IEnumerable<string> GetSectionScopes(string sectionName)
+        {
+            lock (_fileLock)
+            {
+                IEnumerable<IniSection> sections = _iniFile.Sections
+                    .Where(x => StringComparer.OrdinalIgnoreCase.Equals(x.Name, sectionName));
+                foreach (var section in sections)
+                {
+                    yield return section.Scope;
+                }
+            }
+        }
+
+        public bool TryGetValue(string key, out string value)
+        {
+            lock (_fileLock)
+            {
+                value = null;
+
+                if (!TrySplitKey(key, out string section, out string scope, out string property))
+                {
+                    throw new ArgumentException($"Invalid key '{key}'.", nameof(key));
+                }
+
+                return _iniFile.TryGetValue(section, scope, property, out value);
+            }
+        }
+
+        public void SetValue(string key, string value)
+        {
+            lock (_fileLock)
+            {
+                if (!TrySplitKey(key, out string section, out string scope, out string property))
+                {
+                    throw new ArgumentException($"Invalid key '{key}'.", nameof(key));
+                }
+
+                _iniFile.SetValue(section, scope, property, value);
+            }
+        }
+
+        public void Remove(string key)
+        {
+            lock (_fileLock)
+            {
+                if (!TrySplitKey(key, out string section, out string scope, out string property))
+                {
+                    throw new ArgumentException($"Invalid key '{key}'.", nameof(key));
+                }
+
+                _iniFile.UnsetValue(section, scope, property);
+            }
+        }
+
+        private static bool TrySplitKey(string key, out string section, out string scope, out string property)
+        {
+            section = null;
+            scope = null;
+            property = null;
+
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                return false;
+            }
+
+            int first = key.IndexOf('.');
+            int last = key.LastIndexOf('.');
+
+            if (first < 0 || last < 0)
+            {
+                return false;
+            }
+
+            // section.property
+            if (first == last)
+            {
+                section = key.Substring(0, first);
+                property = key.Substring(last + 1);
+
+                return true;
+            }
+
+            // section.scope.maybe.with.periods.property
+            section = key.Substring(0, first);
+            scope = key.Substring(first + 1, last - first - 1);
+            property = key.Substring(last + 1);
+
+            return true;
+        }
+    }
+}

src/shared/TestInfrastructure/Objects/InMemoryIniStore.cs
@@ -1,0 +1,99 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Git.CredentialManager.Tests.Objects
+{
+    public class InMemoryIniStore : IScopedTransactionalStore
+    {
+        private readonly object _txLock = new object();
+
+        public IDictionary<string, string> PersistedStore { get; }
+
+        public IDictionary<string, string> MemoryStore { get; }
+
+        public InMemoryIniStore() : this(EqualityComparer<string>.Default) { }
+
+        public InMemoryIniStore(IEqualityComparer<string> comparer)
+            : this(new Dictionary<string, string>(comparer)) { }
+
+        public InMemoryIniStore(Dictionary<string, string> persistedStore)
+        {
+            PersistedStore = persistedStore;
+            MemoryStore = new Dictionary<string, string>(persistedStore.Comparer);
+
+            Reload();
+        }
+
+        public Task Reload()
+        {
+            // Copy state from persisted store -> memory store
+            lock (_txLock)
+            {
+                MemoryStore.Clear();
+                foreach (var kvp in PersistedStore)
+                {
+                    MemoryStore.Add(kvp);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task Commit()
+        {
+            // Copy state from memory store -> persisted store
+            lock (_txLock)
+            {
+                PersistedStore.Clear();
+                foreach (var kvp in MemoryStore)
+                {
+                    PersistedStore.Add(kvp);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public IEnumerable<string> GetSectionScopes(string sectionName)
+        {
+            var scopes = new HashSet<string>();
+
+            string prefix = $"{sectionName}.";
+            var cmp = StringComparison.OrdinalIgnoreCase;
+            foreach (string key in MemoryStore.Keys.Where(x => x.StartsWith(prefix, cmp)))
+            {
+                int sectionIndex = key.IndexOf(".", cmp);
+                int propertyIndex = key.LastIndexOf(".", cmp);
+
+                if (sectionIndex > -1 && propertyIndex > -1)
+                {
+                    string scope = key.Substring(sectionIndex + 1,
+                        propertyIndex - sectionIndex - 1);
+                    scopes.Add(scope);
+                }
+            }
+
+            return scopes;
+        }
+
+        public bool TryGetValue(string key, out string value)
+        {
+            return MemoryStore.TryGetValue(key, out value);
+        }
+
+        public void SetValue(string key, string value)
+        {
+            MemoryStore[key] = value;
+        }
+
+        public void Remove(string key)
+        {
+            MemoryStore.Remove(key);
+        }
+    }
+}