cbc462b794cdbad8b77ba137102cf8e86891f37b
Matt Cooper <mattc@xbox.com>
2021-02-12 15:39:39 -0500

    implement credential-cache as backing store

--- src/shared/Microsoft.Git.CredentialManager/CredentialCacheStore.cs
+++ src/shared/Microsoft.Git.CredentialManager/CredentialCacheStore.cs
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-using System;
+using System.Collections.Generic;
 
 namespace Microsoft.Git.CredentialManager
 {
@@ -19,19 +19,63 @@ namespace Microsoft.Git.CredentialManager
 
         public ICredential Get(string service, string account)
         {
-            throw new NotImplementedException();
+            var input = MakeGitCredentialsEntry(service, account);
+
+            var result = _helper.InvokeAsync(
+                _git.GitPath,
+                "credential-cache get",
+                input
+            ).GetAwaiter().GetResult();
+
+            if (result.ContainsKey("username") && result.ContainsKey("password"))
+            {
+                return new GitCredential(result["username"], result["password"]);
+            }
+
+            return null;
         }
 
         public void AddOrUpdate(string service, string account, string secret)
         {
-            throw new NotImplementedException();
+            var input = MakeGitCredentialsEntry(service, account);
+            input["password"] = secret;
+
+            // per https://git-scm.com/docs/gitcredentials :
+            // For a store or erase operation, the helper’s output is ignored.
+            _helper.InvokeAsync(
+                _git.GitPath,
+                "credential-cache store",
+                input
+            ).GetAwaiter().GetResult();
         }
 
         public bool Remove(string service, string account)
         {
-            throw new NotImplementedException();
+            var input = MakeGitCredentialsEntry(service, account);
+
+            // per https://git-scm.com/docs/gitcredentials :
+            // For a store or erase operation, the helper’s output is ignored.
+            _helper.InvokeAsync(
+                _git.GitPath,
+                "credential-cache erase",
+                input
+            ).GetAwaiter().GetResult();
+
+            // the credential cache doesn't tell us whether anything was erased
+            // but we're optimistic sorts
+            return true;
         }
 
         #endregion
+
+        private Dictionary<string, string> MakeGitCredentialsEntry(string service, string account)
+        {
+            var result = new Dictionary<string, string>();
+
+            result["url"] = service;
+            // TODO: "account" is unused, is that right?
+
+            return result;
+        }
     }
 }
