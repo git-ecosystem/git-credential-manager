5dda6a61fbaa9cdcd988ba8746c2fc11ebb25aea
Matthew John Cheetham <mjcheetham@outlook.com>
2021-03-24 09:15:59 +0000

    git: drop config --show-scope option usage
    The Git config `--show-scope` option was only introduced in Git from
    version 2.26 onwards. The latest version of Git available in some
    distributions of Linux (or macOS) is often older.
    
    We only needed to know the scope of configuration values in one
    particular call site: reading all Azure Repos user bindinds.
    
    Replace the single `IGitConfiguration::Enumerate` call with two calls to
    `Enumerate`, one for the global scope, and one for the local one.
    
    Drop the --show-scope option parsing.

--- src/shared/Microsoft.Git.CredentialManager/GitConfigurationEntry.cs
+++ src/shared/Microsoft.Git.CredentialManager/GitConfigurationEntry.cs
@@ -4,14 +4,12 @@ namespace Microsoft.Git.CredentialManager
 {
     public class GitConfigurationEntry
     {
-        public GitConfigurationEntry(GitConfigurationLevel level, string key, string value)
+        public GitConfigurationEntry(string key, string value)
         {
-            Level = level;
             Key = key;
             Value = value;
         }
 
-        public GitConfigurationLevel Level { get; }
         public string Key { get; }
         public string Value { get; }
     }
