3b8630fc4da476a3685a93ad311b627d6ad228b9
Matthew John Cheetham <mjcheetham@outlook.com>
2021-05-05 13:31:23 +0100

    oauth: extract OpenDefaultBrowser to reusable component
    Extract the `OpenDefaultBrowser` method to a helper/util class so it can
    be used by other components.

--- src/shared/Microsoft.Git.CredentialManager/Authentication/OAuth/OAuth2SystemWebBrowser.cs
+++ src/shared/Microsoft.Git.CredentialManager/Authentication/OAuth/OAuth2SystemWebBrowser.cs
@@ -73,63 +73,9 @@ namespace Microsoft.Git.CredentialManager.Authentication.OAuth
 
             Task<Uri> interceptTask = InterceptRequestsAsync(redirectUri, ct);
 
-            OpenDefaultBrowser(authorizationUri);
+            BrowserUtils.OpenDefaultBrowser(_environment, authorizationUri);
 
             return await interceptTask;
-        }
-
-        private void OpenDefaultBrowser(Uri uri)
-        {
-            if (!uri.Scheme.Equals(Uri.UriSchemeHttp, StringComparison.OrdinalIgnoreCase) &&
-                !uri.Scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase))
-            {
-                throw new ArgumentException("Can only open HTTP/HTTPS URIs", nameof(uri));
-            }
-
-            string url = uri.ToString();
-
-            ProcessStartInfo psi = null;
-            if (PlatformUtils.IsLinux())
-            {
-                // On Linux, 'shell execute' utilities like xdg-open launch a process without
-                // detaching from the standard in/out descriptors. Some applications (like
-                // Chromium) write messages to stdout, which is currently hooked up and being
-                // consumed by Git, and cause errors.
-                //
-                // Sadly, the Framework does not allow us to redirect standard streams if we
-                // set ProcessStartInfo::UseShellExecute = true, so we must manually launch
-                // these utilities and redirect the standard streams manually.
-                //
-                // We try and use the same 'shell execute' utilities as the Framework does,
-                // searching for them in the same order until we find one.
-                foreach (string shellExec in new[] { "xdg-open", "gnome-open", "kfmclient" })
-                {
-                    if (_environment.TryLocateExecutable(shellExec, out string shellExecPath))
-                    {
-                        psi = new ProcessStartInfo(shellExecPath, url)
-                        {
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true
-                        };
-
-                        // We found a way to open the URI; stop searching!
-                        break;
-                    }
-                }
-
-                if (psi is null)
-                {
-                    throw new Exception("Failed to locate a utility to launch the default web browser.");
-                }
-            }
-            else
-            {
-                // On Windows and macOS, `ShellExecute` and `/usr/bin/open` disconnect the child process
-                // from our standard in/out streams, so we can just use the Framework to do this.
-                psi = new ProcessStartInfo(url) {UseShellExecute = true};
-            }
-
-            Process.Start(psi);
         }
 
         private async Task<Uri> InterceptRequestsAsync(Uri listenUri, CancellationToken ct)
