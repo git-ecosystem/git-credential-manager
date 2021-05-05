1ecb3e13bb5951aba0ae3bf310ae94d34d9cc059
Matthew John Cheetham <mjcheetham@outlook.com>
2021-05-05 13:35:45 +0100

    xui: add shared Avalonia UI class library
    Add a new project to host all the shared components for Avalonia-based
    UI helpers.

---
+++ src/shared/Microsoft.Git.CredentialManager.UI/AvaloniaApp.axaml.cs
@@ -0,0 +1,39 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace Microsoft.Git.CredentialManager.UI
+{
+    public class AvaloniaApp : Avalonia.Application
+    {
+        private readonly Func<Window> _mainWindowFunc;
+
+        public AvaloniaApp() { }
+
+        public AvaloniaApp(Func<Window> mainWindowFunc)
+        {
+            _mainWindowFunc = mainWindowFunc;
+        }
+
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop && _mainWindowFunc != null)
+            {
+                desktop.MainWindow = _mainWindowFunc();
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+
+        private void About(object sender, EventArgs e)
+        {
+            // TODO
+        }
+    }
+}
