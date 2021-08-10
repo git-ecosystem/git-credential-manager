03bf5d193bcd46001abae624fdc9c95d12622037
Victoria Dye <vdye@github.com>
2021-08-10 17:50:04 -0400

    fixup! linux: add option for GCM_GPG_PATH environment variable
    Whitespace cleanup

--- src/shared/Microsoft.Git.CredentialManager/CommandContext.cs
+++ src/shared/Microsoft.Git.CredentialManager/CommandContext.cs
@@ -119,7 +119,6 @@ namespace Microsoft.Git.CredentialManager
                                         );
                 Settings          = new Settings(Environment, Git);
                 CredentialStore   = new MacOSKeychain(Settings.CredentialNamespace);
-
             }
             else if (PlatformUtils.IsLinux())
             {
@@ -136,7 +135,6 @@ namespace Microsoft.Git.CredentialManager
                                             FileSystem.GetCurrentDirectory()
                                         );
                 Settings          = new Settings(Environment, Git);
-
                 string gpgPath    = GetGpgPath(Environment, FileSystem, Trace);
                 IGpg gpg          = new Gpg(gpgPath, SessionManager);
                 CredentialStore   = new LinuxCredentialStore(FileSystem, Settings, SessionManager, gpg, Environment, Git);
