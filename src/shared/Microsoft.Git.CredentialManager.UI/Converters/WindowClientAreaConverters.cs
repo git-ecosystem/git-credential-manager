819c7dd8a171c0a657842c5ba213dc343934c19e
Matthew John Cheetham <mjcheetham@outlook.com>
2021-09-23 11:13:47 +0100

    xui: remove Avalonia deps in WindowViewModel
    Remove the direct dependency on Avalonia types in the base
    WindowViewModel. This will allow us later to extract UI framework
    agnostic components, to be shared between WPF and Avalonia
    implementations of the UI helpers.

---
+++ src/shared/Microsoft.Git.CredentialManager.UI/Converters/WindowClientAreaConverters.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Platform;
+
+namespace Microsoft.Git.CredentialManager.UI.Converters
+{
+    public static class WindowClientAreaConverters
+    {
+        public static readonly IValueConverter BoolToChromeHints =
+            new FuncValueConverter<bool, ExtendClientAreaChromeHints>(
+                x => x
+                    ? ExtendClientAreaChromeHints.NoChrome
+                    : ExtendClientAreaChromeHints.PreferSystemChrome);
+    }
+}
