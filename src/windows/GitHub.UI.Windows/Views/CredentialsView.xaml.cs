14c9cd04f317a4691b282e88175c075922335da0
Matthew John Cheetham <mjcheetham@outlook.com>
2021-09-29 11:16:43 +0100

    github: reimplement credential and 2FA WPF views
    Reimplement WPF views for the credential and 2FA GitHub prompts.
    The layout is now much simpler, and matches the Avalonia-based UI.

--- src/windows/GitHub.UI.Windows/Views/CredentialsView.xaml.cs
+++ src/windows/GitHub.UI.Windows/Views/CredentialsView.xaml.cs
@@ -1,15 +1,78 @@
-using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Threading;
 using GitHub.UI.ViewModels;
+using Microsoft.Git.CredentialManager.UI.Controls;
 
 namespace GitHub.UI.Views
 {
-    public partial class CredentialsView : UserControl
+    public partial class CredentialsView : UserControl, IFocusable
     {
         public CredentialsView()
         {
             InitializeComponent();
         }
+
+        // Set focus on a UIElement the next time it becomes visible
+        private static void OnIsVisibleChangedOneTime(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is UIElement element)
+            {
+                // Unsubscribe to prevent re-triggering
+                element.IsVisibleChanged -= OnIsVisibleChangedOneTime;
+
+                // Set logical focus
+                element.Focus();
+
+                // Set keyboard focus
+                Keyboard.Focus(element);
+            }
+        }
+
+        public void SetFocus()
+        {
+            if (!(DataContext is CredentialsViewModel vm))
+            {
+                return;
+            }
+
+            //
+            // Select the best available authentication mechanism that is visible
+            // and make the textbox/button focused when it next made visible.
+            //
+            // In WPF the controls in a TabItem are not part of the visual tree until
+            // the TabControl has been switched to that tab, so we must delay focusing
+            // on the textbox/button until it becomes visible.
+            //
+            // This means as the user first moves through the tabs, the "correct" control
+            // will be given focus in that tab.
+            //
+            void SetFocusOnNextVisible(UIElement element)
+            {
+                element.IsVisibleChanged += OnIsVisibleChangedOneTime;
+            }
+
+            // Set up focus events on all controls
+            SetFocusOnNextVisible(
+                string.IsNullOrWhiteSpace(vm.UserName)
+                    ? userNameTextBox
+                    : passwordTextBox);
+            SetFocusOnNextVisible(tokenTextBox);
+            SetFocusOnNextVisible(browserButton);
+
+            // Switch to the preferred tab
+            if (vm.ShowBrowserLogin)
+            {
+                tabControl.SelectedIndex = 0;
+            }
+            else if (vm.ShowTokenLogin)
+            {
+                tabControl.SelectedIndex = 1;
+            }
+            else if (vm.ShowBasicLogin)
+            {
+                tabControl.SelectedIndex = 2;
+            }
+        }
+    }
 }
--- src/windows/GitHub.UI.Windows/Views/TwoFactorView.xaml.cs
+++ src/windows/GitHub.UI.Windows/Views/TwoFactorView.xaml.cs
@@ -1,14 +1,28 @@
 using System;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using Microsoft.Git.CredentialManager.UI.Controls;
 
 namespace GitHub.UI.Views
 {
-    public partial class TwoFactorView : UserControl
+    public partial class TwoFactorView : UserControl, IFocusable
     {
         public TwoFactorView()
         {
             InitializeComponent();
+
+            IsVisibleChanged += (s, e) =>
+            {
+                if (IsVisible)
+                {
+                    Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, (Action)SetFocus);
+                }
+            };
+        }
+
+        public void SetFocus()
+        {
+            codeInput.SetFocus();
         }
     }
 }
