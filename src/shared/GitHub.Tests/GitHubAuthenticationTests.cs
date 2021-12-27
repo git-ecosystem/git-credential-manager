e2cff8f5a204e44fcb82d7530610260d4657ad77
matthickford <mirth.hickford@gmail.com>
2021-12-27 23:38:45 +0000

    Remove overzealous ThrowIfTerminalPromptsDisabled check

--- src/shared/GitHub.Tests/GitHubAuthenticationTests.cs
+++ src/shared/GitHub.Tests/GitHubAuthenticationTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using GitCredentialManager.Tests.Objects;
+using Moq;
 using Xunit;
 
 namespace GitHub.Tests
@@ -15,6 +16,19 @@ namespace GitHub.Tests
             await Assert.ThrowsAsync<ArgumentException>("modes",
                 () => auth.GetAuthenticationAsync(null, null, AuthenticationModes.None)
             );
+        }
+
+        [Theory]
+        [InlineData(AuthenticationModes.Browser)]
+        [InlineData(AuthenticationModes.Device)]
+        public async Task GitHubAuthentication_GetAuthenticationAsync_TerminalPromptNotRequired(GitHub.AuthenticationModes modes)
+        {
+            var context = new TestCommandContext();
+            context.Settings.IsTerminalPromptsEnabled = false;
+            context.SessionManager.IsDesktopSession = true;
+            var auth = new GitHubAuthentication(context);
+            var result = await auth.GetAuthenticationAsync(null, null, modes);
+            Assert.Equal(modes, result.AuthenticationMode);
         }
     }
 }
