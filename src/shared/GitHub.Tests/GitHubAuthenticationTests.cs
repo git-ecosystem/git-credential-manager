b9cfa9b31d60c4445cb705775b0a71eb345febc0
M Hickford <mirth.hickford@gmail.com>
2021-12-28 16:35:54 +0000

    more unit tests

--- src/shared/GitHub.Tests/GitHubAuthenticationTests.cs
+++ src/shared/GitHub.Tests/GitHubAuthenticationTests.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using GitCredentialManager.Tests.Objects;
 using Moq;
+using Moq.Protected;
 using Xunit;
 
 namespace GitHub.Tests
@@ -19,30 +22,25 @@ namespace GitHub.Tests
         }
 
         [Theory]
-        [InlineData(AuthenticationModes.Browser, true)]
-        [InlineData(AuthenticationModes.Browser, false)]
-        [InlineData(AuthenticationModes.Device, true)]
-        [InlineData(AuthenticationModes.Device, false)]
-        public async Task GitHubAuthentication_GetAuthenticationAsync_SingleChoice_TerminalAndInteractionNotRequired(GitHub.AuthenticationModes modes, bool useHelper)
+        [InlineData(AuthenticationModes.Browser)]
+        [InlineData(AuthenticationModes.Device)]
+        public async Task GitHubAuthentication_GetAuthenticationAsync_SingleChoice_TerminalAndInteractionNotRequired(GitHub.AuthenticationModes modes)
         {
             var context = new TestCommandContext();
             context.Settings.IsTerminalPromptsEnabled = false;
             context.Settings.IsInteractionAllowed = false;
             context.SessionManager.IsDesktopSession = true; // necessary for browser
-            if (useHelper)
-            {
-                context.FileSystem.Files["/usr/local/bin/GitHub.UI"] = new byte[0];
-            }
+            context.FileSystem.Files["/usr/local/bin/GitHub.UI"] = new byte[0];
+            context.FileSystem.Files[@"C:\Program Files\Git Credential Manager Core\GitHub.UI.exe"] = new byte[0];
             var auth = new GitHubAuthentication(context);
-            var result = await auth.GetAuthenticationAsync(new Uri("https://github.com"), null, modes);
+            var result = await auth.GetAuthenticationAsync(null, null, modes);
             Assert.Equal(modes, result.AuthenticationMode);
         }
 
         [Fact]
-        public async Task GitHubAuthentication_GetAuthenticationAsync_NonDesktopSession_RequiresTerminal()
+        public async Task GitHubAuthentication_GetAuthenticationAsync_TerminalPromptsDisabled_Throws()
         {
             var context = new TestCommandContext();
-            context.FileSystem.Files["/usr/local/bin/GitHub.UI"] = new byte[0];
             context.Settings.IsTerminalPromptsEnabled = false;
             var auth = new GitHubAuthentication(context);
             var exception = await Assert.ThrowsAsync<InvalidOperationException>(
@@ -51,18 +49,51 @@ namespace GitHub.Tests
             Assert.Equal("Cannot prompt because terminal prompts have been disabled.", exception.Message);
         }
 
+        // reproduces https://github.com/GitCredentialManager/git-credential-manager/issues/453
         [Fact]
-        public async Task GitHubAuthentication_GetAuthenticationAsync_DesktopSession_RequiresInteraction()
+        public async Task GitHubAuthentication_GetAuthenticationAsync_Terminal()
         {
             var context = new TestCommandContext();
             context.FileSystem.Files["/usr/local/bin/GitHub.UI"] = new byte[0];
-            context.SessionManager.IsDesktopSession = true;
+            context.FileSystem.Files[@"C:\Program Files\Git Credential Manager Core\GitHub.UI.exe"] = new byte[0];
+            var auth = new GitHubAuthentication(context);
+            context.Terminal.Prompts["option (enter for default)"] = "";
+            var result = await auth.GetAuthenticationAsync(null, null, AuthenticationModes.All);
+            Assert.Equal(AuthenticationModes.Device, result.AuthenticationMode);
+        }
+
+        [Fact]
+        public async Task GitHubAuthentication_GetAuthenticationAsync_AuthenticationModesAll_RequiresInteraction()
+        {
+            var context = new TestCommandContext();
             context.Settings.IsInteractionAllowed = false;
             var auth = new GitHubAuthentication(context);
             var exception = await Assert.ThrowsAsync<InvalidOperationException>(
                 () => auth.GetAuthenticationAsync(new Uri("https://github.com"), null, AuthenticationModes.All)
             );
             Assert.Equal("Cannot prompt because user interactivity has been disabled.", exception.Message);
+        }
+
+        [Fact]
+        public async Task GitHubAuthentication_GetAuthenticationAsync_Helper_Basic()
+        {
+            var context = new TestCommandContext();
+            context.FileSystem.Files["/usr/local/bin/GitHub.UI"] = new byte[0];
+            context.FileSystem.Files[@"C:\Program Files\Git Credential Manager Core\GitHub.UI.exe"] = new byte[0];
+            context.SessionManager.IsDesktopSession = true;
+            var auth = new Mock<GitHubAuthentication>(MockBehavior.Strict, context);
+            auth.Setup(x => x.InvokeHelperAsync(It.IsAny<string>(), "prompt --all", It.IsAny<IDictionary<string, string>>(), It.IsAny<System.Threading.CancellationToken>()))
+            .Returns(Task.FromResult<IDictionary<string, string>>(
+                new Dictionary<string, string>
+                {
+                    ["mode"] = "basic",
+                    ["username"] = "tim",
+                    ["password"] = "hunter2"
+                }));
+            var result = await auth.Object.GetAuthenticationAsync(new Uri("https://github.com"), null, AuthenticationModes.All);
+            Assert.Equal(AuthenticationModes.Basic, result.AuthenticationMode);
+            Assert.Equal("tim", result.Credential.Account);
+            Assert.Equal("hunter2", result.Credential.Password);
         }
     }
 }
--- src/shared/GitHub/GitHubAuthentication.cs
+++ src/shared/GitHub/GitHubAuthentication.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
