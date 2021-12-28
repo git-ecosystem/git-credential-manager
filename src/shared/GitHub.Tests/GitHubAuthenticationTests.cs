30d50e5490883684004759d07fc17aa75453a40a
matthickford <mirth.hickford@gmail.com>
2021-12-28 00:14:24 +0000

    Unit test for #453
    Remove overzealous interactivity checks.

--- src/shared/GitHub.Tests/GitHubAuthenticationTests.cs
+++ src/shared/GitHub.Tests/GitHubAuthenticationTests.cs
@@ -30,5 +30,32 @@ namespace GitHub.Tests
             var result = await auth.GetAuthenticationAsync(null, null, modes);
             Assert.Equal(modes, result.AuthenticationMode);
         }
+
+        [Fact]
+        public async Task GitHubAuthentication_GetAuthenticationAsync_NonDesktopSession_RequiresTerminal()
+        {
+            var context = new TestCommandContext();
+            context.FileSystem.Files["/usr/local/bin/GitHub.UI"] = new byte[0];
+            context.Settings.IsTerminalPromptsEnabled = false;
+            var auth = new GitHubAuthentication(context);
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(
+                () => auth.GetAuthenticationAsync(null, null, AuthenticationModes.All)
+            );
+            Assert.Equal("Cannot prompt because terminal prompts have been disabled.", exception.Message);
+        }
+
+        [Fact]
+        public async Task GitHubAuthentication_GetAuthenticationAsync_DesktopSession_RequiresInteraction()
+        {
+            var context = new TestCommandContext();
+            context.FileSystem.Files["/usr/local/bin/GitHub.UI"] = new byte[0];
+            context.SessionManager.IsDesktopSession = true;
+            context.Settings.IsInteractionAllowed = false;
+            var auth = new GitHubAuthentication(context);
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(
+                () => auth.GetAuthenticationAsync(new Uri("https://github.com"), null, AuthenticationModes.All)
+            );
+            Assert.Equal("Cannot prompt because user interactivity has been disabled.", exception.Message);
+        }
     }
 }
