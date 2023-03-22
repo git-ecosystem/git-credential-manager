7abcfef926d9d7a11fd66825d5e8993acdd22232
Lessley Dennington <ldennington@github.com>
2023-03-22 15:23:27 -0600

    trace2: update span handling
    Previously, we only set up custom performance format span handling for
    time-related information (see [1]). However, with the upcoming
    introduction of regions, we will need custom spans for the category and
    repo strings. This change refactors and adds to the current logic to
    correctly handle all 3 span types (i.e. time, repo, and category).
    
    [1] 59a2692

--- src/shared/Core.Tests/Trace2MessageTests.cs
+++ src/shared/Core.Tests/Trace2MessageTests.cs
@@ -6,12 +6,33 @@ namespace Core.Tests;
 public class Trace2MessageTests
 {
     [Theory]
-    [InlineData(0.013772,    "  0.013772 ")]
-    [InlineData(26.316083,   " 26.316083 ")]
-    [InlineData(100.316083,  "100.316083 ")]
+    [InlineData(0.013772,     "  0.013772 ")]
+    [InlineData(26.316083,    " 26.316083 ")]
+    [InlineData(100.316083,   "100.316083 ")]
+    [InlineData(1000.316083,  "1000.316083")]
     public void BuildTimeSpan_Match_Returns_Expected_String(double input, string expected)
     {
         var actual = Trace2Message.BuildTimeSpan(input);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void BuildRepoSpan_Match_Returns_Expected_String()
+    {
+        var input = 1;
+        var expected = " r1  ";
+        var actual = Trace2Message.BuildRepoSpan(input);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("foo",               " foo         ")]
+    [InlineData("foobar",            " foobar      ")]
+    [InlineData("foo_bar_baz",       " foo_bar_baz ")]
+    [InlineData("foobarbazfoo",      " foobarbazfo ")]
+    public void BuildCategorySpan_Match_Returns_Expected_String(string input, string expected)
+    {
+        var actual = Trace2Message.BuildCategorySpan(input);
         Assert.Equal(expected, actual);
     }
 }
