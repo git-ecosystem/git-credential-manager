59d153a14be6c6f54a6a6561a141d2607f7b2982
Matthew John Cheetham <mjcheetham@outlook.com>
2023-05-11 12:27:49 -0700

    trace: trace all exceptions

--- src/shared/Core/Application.cs
+++ src/shared/Core/Application.cs
@@ -149,6 +149,9 @@ namespace GitCredentialManager
                     break;
             }
 
+            // Write the exception to the trace log
+            Context.Trace.WriteException(ex);
+
             // If tracing is enabled then also print the stack trace to stderr
             bool printStack = Context.Settings.GetTracingEnabled(out _);
             if (printStack)
