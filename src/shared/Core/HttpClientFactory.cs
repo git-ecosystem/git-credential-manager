a656832ff360c66313d0b669e27eebf22cb44e30
miya789 <31382715+miya789@users.noreply.github.com>
2023-06-06 18:58:42 +0900

    http: fix the namespace of `CurlCookie.cs`
    Co-authored-by: Matthew John Cheetham <mjcheetham@github.com>

--- src/shared/Core/HttpClientFactory.cs
+++ src/shared/Core/HttpClientFactory.cs
@@ -7,7 +7,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
-using CurlCookie;
 
 namespace GitCredentialManager
 {
