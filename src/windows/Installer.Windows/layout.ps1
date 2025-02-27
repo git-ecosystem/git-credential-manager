c6ad8f532f59c1aad61ccf042b39f8c7be0a0f9c
Dennis Ameling <dennis@dennisameling.com>
2025-02-27 12:05:09 +0100

    Add support for Windows x64 and arm64 builds
    Previously, GCM was only built for x86 on Windows. This commit
    adds support for building for and on Windows x64 and arm64.
    
    It builds the host architecture by default and supports
    explicitly specifying the target architecture.
    
    Signed-off-by: Dennis Ameling <dennis@dennisameling.com>

--- src/windows/Installer.Windows/layout.ps1
+++ src/windows/Installer.Windows/layout.ps1
@@ -1,7 +1,28 @@
 # Inputs
-param ([Parameter(Mandatory)] $Configuration, [Parameter(Mandatory)] $Output, $SymbolOutput)
+param ([Parameter(Mandatory)] $Configuration, [Parameter(Mandatory)] $Output, $RuntimeIdentifier, $SymbolOutput)
 
 Write-Output "Output: $Output"
+
+# Determine a runtime if one was not provided
+if (-not $RuntimeIdentifier) {
+    $arch = $env:PROCESSOR_ARCHITECTURE
+    switch ($arch) {
+        "AMD64" { $RuntimeIdentifier = "win-x64" }
+        "x86"   { $RuntimeIdentifier = "win-x86" }
+        "ARM64" { $RuntimeIdentifier = "win-arm64" }
+        default {
+            Write-Host "Unknown architecture: $arch"
+            exit 1
+        }
+    }
+}
+
+Write-Output "Building for runtime '$RuntimeIdentifier'"
+
+if ($RuntimeIdentifier -ne 'win-x86' -and $RuntimeIdentifier -ne 'win-x64' -and $RuntimeIdentifier -ne 'win-arm64') {
+    Write-Host "Unsupported RuntimeIdentifier: $RuntimeIdentifier"
+    exit 1
+}
 
 # Directories
 $THISDIR = $PSScriptRoot
@@ -41,27 +62,55 @@ Write-Output "Publishing core application..."
 dotnet publish "$GCM_SRC" `
 	--framework net472 `
 	--configuration "$Configuration" `
-	--runtime win-x86 `
+	--runtime $RuntimeIdentifier `
 	--output "$PAYLOAD"
 
 # Delete libraries that are not needed for Windows but find their way
 # into the publish output.
 Remove-Item -Path "$PAYLOAD/*.dylib" -Force -ErrorAction Ignore
 
-# Delete extraneous files that get included for other architectures
-# We only care about x86 as the core GCM executable is only targeting x86
-Remove-Item -Path "$PAYLOAD/arm/" -Recurse -Force -ErrorAction Ignore
-Remove-Item -Path "$PAYLOAD/arm64/" -Recurse -Force -ErrorAction Ignore
-Remove-Item -Path "$PAYLOAD/x64/" -Recurse -Force -ErrorAction Ignore
+# Delete extraneous files that get included for other runtimes
 Remove-Item -Path "$PAYLOAD/musl-x64/" -Recurse -Force -ErrorAction Ignore
-Remove-Item -Path "$PAYLOAD/runtimes/win-arm64/" -Recurse -Force -ErrorAction Ignore
-Remove-Item -Path "$PAYLOAD/runtimes/win-x64/" -Recurse -Force -ErrorAction Ignore
 
-# The Avalonia and MSAL binaries in these directories are already included in
-# the $PAYLOAD directory directly, so we can delete these extra copies.
-Remove-Item -Path "$PAYLOAD/x86/libSkiaSharp.dll" -Recurse -Force -ErrorAction Ignore
-Remove-Item -Path "$PAYLOAD/x86/libHarfBuzzSharp.dll" -Recurse -Force -ErrorAction Ignore
-Remove-Item -Path "$PAYLOAD/runtimes/win-x86/native/msalruntime_x86.dll" -Recurse -Force -ErrorAction Ignore
+switch ($RuntimeIdentifier) {
+    "win-x86" {
+        Remove-Item -Path "$PAYLOAD/arm/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/arm64/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/x64/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-arm64/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-x64/" -Recurse -Force -ErrorAction Ignore
+        # The Avalonia and MSAL binaries are already included in the $PAYLOAD directory directly
+        Remove-Item -Path "$PAYLOAD/x86/libSkiaSharp.dll" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/x86/libHarfBuzzSharp.dll" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-x86/native/msalruntime_x86.dll" -Force -ErrorAction Ignore
+    }
+    "win-x64" {
+        Remove-Item -Path "$PAYLOAD/arm/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/arm64/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/x86/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-arm64/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-x86/" -Recurse -Force -ErrorAction Ignore
+        # The Avalonia and MSAL binaries are already included in the $PAYLOAD directory directly
+        Remove-Item -Path "$PAYLOAD/x64/libSkiaSharp.dll" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/x64/libHarfBuzzSharp.dll" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/x64/libSkiaSharp.so" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/x64/libHarfBuzzSharp.so" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-x64/native/msalruntime.dll" -Force -ErrorAction Ignore
+    }
+    "win-arm64" {
+        Remove-Item -Path "$PAYLOAD/arm/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/x86/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/x64/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-x86/" -Recurse -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-x64/" -Recurse -Force -ErrorAction Ignore
+        # The Avalonia and MSAL binaries are already included in the $PAYLOAD directory directly
+        Remove-Item -Path "$PAYLOAD/arm64/libSkiaSharp.dll" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/arm64/libHarfBuzzSharp.dll" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/arm64/libSkiaSharp.so" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/arm64/libHarfBuzzSharp.so" -Force -ErrorAction Ignore
+        Remove-Item -Path "$PAYLOAD/runtimes/win-arm64/native/msalruntime_arm64.dll" -Force -ErrorAction Ignore
+    }
+}
 
 # Delete localized resource assemblies - we don't localize the core GCM assembly anyway
 Get-ChildItem "$PAYLOAD" -Recurse -Include "*.resources.dll" | Remove-Item -Force -ErrorAction Ignore
