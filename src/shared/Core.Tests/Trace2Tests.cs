d4e2f59030b0862a7d0d01b96fad454f27d5932e
Matthew John Cheetham <mjcheetham@outlook.com>
2025-04-30 15:17:44 +0100

    trace2: fix pipe/socket name parsing
    There are a few bugs in the way we read a pipe or socket name for the
    TRACE2 event target. We do not correctly trim the \\.\pipe\ prefix on
    Windows, if present. On Unix we do not correctly strip the af_unix:
    prefix correctly if a socket file path contains a ':'.
    
    Let's fix this!
    
    Signed-off-by: Matthew John Cheetham <mjcheetham@outlook.com>

--- src/shared/Core.Tests/Trace2Tests.cs
+++ src/shared/Core.Tests/Trace2Tests.cs
@@ -6,25 +6,26 @@ public class Trace2Tests
 {
     [PosixTheory]
     [InlineData("af_unix:foo", "foo")]
-    [InlineData("af_unix:stream:foo-bar", "foo-bar")]
-    [InlineData("af_unix:dgram:foo-bar-baz", "foo-bar-baz")]
+    [InlineData("af_unix:foo/bar", "foo/bar")]
+    [InlineData("af_unix:stream:foo/bar", "foo/bar")]
+    [InlineData("af_unix:dgram:foo/bar/baz", "foo/bar/baz")]
     public void TryGetPipeName_Posix_Returns_Expected_Value(string input, string expected)
     {
         var isSuccessful = Trace2.TryGetPipeName(input, out var actual);
 
         Assert.True(isSuccessful);
-        Assert.Matches(actual, expected);
+        Assert.Equal(actual, expected);
     }
 
     [WindowsTheory]
     [InlineData("\\\\.\\pipe\\git-foo", "git-foo")]
     [InlineData("\\\\.\\pipe\\git-foo-bar", "git-foo-bar")]
-    [InlineData("\\\\.\\pipe\\foo\\git-bar", "git-bar")]
+    [InlineData("\\\\.\\pipe\\foo\\git-bar", "foo\\git-bar")]
     public void TryGetPipeName_Windows_Returns_Expected_Value(string input, string expected)
     {
         var isSuccessful = Trace2.TryGetPipeName(input, out var actual);
 
         Assert.True(isSuccessful);
-        Assert.Matches(actual, expected);
+        Assert.Equal(expected, actual);
     }
 }
